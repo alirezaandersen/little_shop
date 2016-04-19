8bdb44432c23a0875dee06a800b87fd7f3c34c5e
Jonathan <jonacom@lissismore.com>
2016-04-19 11:10:50 -0600

    Add ability to add to cart, add redirect to previous page. Corrected inconsistent shopping cart feature - not being able to add multiple items

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,23 +3,27 @@ class Cart
   attr_reader :contents
 
   def initialize(initial_contents)
-    @contents = initial_contents || {animals: {}, accessories: {}}
+    @contents = initial_contents
   end
 
   def add_animal(animal_id)
-    contents[:animals][animal_id] ||= 0
-    contents[:animals][animal_id] += 1
-    contents[:accessories] ||= {}
+    contents["animals"] ||= {}
+    contents["animals"][animal_id.to_s] ||= 0
+    contents["animals"][animal_id.to_s] += 1
+    contents["accessories"] ||= {}
   end
 
   def add_accessory(accessory_id)
-    contents[:accessories][accessory_id] ||= 0
-    contents[:accessories][accessory_id] += 1
-    contents[:animals] ||= {}
+    contents["accessories"] ||= {}
+    contents["accessories"][accessory_id.to_s] ||= 0
+    contents["accessories"][accessory_id.to_s] += 1
+    contents["animals"] ||= {}
   end
 
   def total
-    @cart ? contents[:animals].values.sum + contents[:accessories].values.sum : 0
+    animals_total = contents["animals"] ? contents["animals"].values.sum : 0
+    accessories_total = contents["accessories"] ? contents["accessories"].values.sum : 0
+    animals_total + accessories_total
   end
 
 end
