fd1a043eaeef95cabbadaff36035a38087ccf61f
Jonathan <jonacom@lissismore.com>
2016-04-20 10:59:57 -0600

    Add top navbar, side navbar and toast for flashes

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -4,4 +4,9 @@ class AnimalsController < ApplicationController
     @animals = Animal.all
   end
 
+  def home
+    @dogs = Animal.all.where(species: "dog").take(4)
+    @cats = Animal.all.where(species: "cat").take(4)
+  end
+
 end
