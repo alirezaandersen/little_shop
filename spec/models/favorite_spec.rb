4da9d215f663aab67fe7fb24d3c70f7d0b99e703
Jonathan <jonacom@lissismore.com>
2016-04-21 20:02:30 -0600

    implement base user dashboard

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Favorite, type: :model do
   context "validations" do
     it { is_expected.to validate_presence_of(:animal_id) }
     it { is_expected.to validate_presence_of(:user_id) }
-      
+
     it "adds animal to contents" do
       cart = Favorite.new(nil)
       animal = FactoryGirl.create(:animal)
